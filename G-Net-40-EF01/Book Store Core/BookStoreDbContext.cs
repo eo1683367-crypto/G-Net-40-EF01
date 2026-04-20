681c82920a0b1c38404ab8d821fe99cdad571559
Softlaptop <Softlaptop@DESKTOP-65SBLOR>
2026-04-20 15:53:24 +0200

    Add Bookstore entities, DbContext, and initial project structure

---
+++ G-Net-40-EF01/Book Store Core/BookStoreDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using G_Net_40_EF01.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace G_Net_40_EF01.Book_Store_Core
+{
+    public class BookStoreDbContext : DbContext
+    {
+        // Configure the database connection string and other options here
+        #region Configure Connection String
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // configure the connection string to your database
+            optionsBuilder.UseSqlServer("Server=.; Database= ReadMoreBooks; trusted_connection= True; trustservercertificate= True;");
+        }
+        #endregion
+
+        // Define your DbSet properties for each entity here
+        #region My DbSet
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        #endregion
+    }
+}
